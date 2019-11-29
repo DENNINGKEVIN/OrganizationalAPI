061cf79277c827f60744a333dfd10c89124a2cec
denningkevin <denningkev@gmail.com>
2019-11-29 15:04:09 +0300

    update dependancies in build.gradle

---
+++ src/test/java/DepartmentsTest.java
@@ -0,0 +1,15 @@
+import org.junit.After;
+import org.junit.Before;
+
+public class DepartmentsTest {
+    public static void main(String[] args) {
+
+        @Before
+        public void setUp() throws Exception {
+        }
+
+        @After
+        public void tearDown() throws Exception {
+        }
+    }
+}
